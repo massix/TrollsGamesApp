f3f7efedabf9b9ef42eefff1742e00f4f76a6088
Massimo Gengarelli <massimo.gengarelli@gmail.com>
2017-11-01 16:55:36 +0100

    RTFM

--- travis-build.sh
+++ travis-build.sh
@@ -3,7 +3,7 @@
 if [[ ${TRAVIS_BRANCH} == "develop" ]]; then
     ./gradlew check assembleStaging
     cp ./app/build/outputs/apk/app-staging.apk TrollsGames.apk
-elif [[ ${TRAVIS_BRANCH} == "master" ]]; then
+elif [[ "x${TRAVIS_TAG}" != "x" ]]; then
     ./gradlew check assembleRelease
     cp ./app/build/outputs/apk/app-release.apk TrollsGames-${TRAVIS_TAG}.apk
 fi
